83d8fd8608f1a5ac237d663a0fbf86fa9ed67151
CAS <csapino@gmail.com>
2014-09-09 21:56:28 +0200

    Revert "Setup de GIT"
    This reverts commit 3238ec802275d75140a1756d44db4476a826377a.

--- PhalanxCommon/CwxException.cs
+++ PhalanxCommon/CwxException.cs
@@ -30,7 +30,11 @@ namespace PhalanxCommon
                 }
             }  
         }
-        // test2 ahora pasa al 3
+<<<<<<< HEAD
+
+=======
+        // test2
+>>>>>>> parent of b90cd10... test3
         public CwxException(string FriendlyMessage)
         {
             _friendly_message = FriendlyMessage;
