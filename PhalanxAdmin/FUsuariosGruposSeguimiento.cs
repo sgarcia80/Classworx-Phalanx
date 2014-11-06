15f50ca006fb802b929e3b90371837e3c3996b3d
CAS <csapino@gmail.com>
2014-11-06 18:34:48 +0100

    Issue #15 - DT2014.01.017.000: Reporte de Usuarios por Grupo de Seguimiento de Solicitudes.

--- PhalanxAdmin/FUsuariosGruposSeguimiento.cs
+++ PhalanxAdmin/FUsuariosGruposSeguimiento.cs
@@ -13,7 +13,7 @@ using System.Collections;
 
 namespace PhalanxAdmin
 {
-    public partial class FUsuariosGruposSeguimiento : PhalanxAdmin.FBaseAuditoria
+    public partial class FUsuariosGruposSeguimiento : PhalanxAdmin.FBaseReportes
     {
         protected IList _entities;
 
