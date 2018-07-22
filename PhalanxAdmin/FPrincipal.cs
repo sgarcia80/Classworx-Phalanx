b038a3e0f4575ab7adee9b7f0433a56c5d0ee6ca
Santiago Garcia <garcia.santiago.daniel@gmail.com>
2018-07-22 17:15:17 -0300

    Se integra el formulario principal

--- PhalanxAdmin/FPrincipal.cs
+++ PhalanxAdmin/FPrincipal.cs
@@ -25,7 +25,7 @@ namespace PhalanxAdmin
 
         private void FPrincipal_Load(object sender, EventArgs e)
         {
-            string Version = "3.17.04.17";
+            string Version = "3.17.9.17";
             this.Text += " v" + Version;
             try
             {
